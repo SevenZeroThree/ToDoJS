5d35a116b0999fb441483776c60e260932860644
Chris <chris@Erica-Houses-MacBook-Pro.local>
2017-09-02 16:47:15 -0400

    plain javascript todos

---
+++ examples/JavaScript/scripts/javascript.js
@@ -0,0 +1,77 @@
+var todos = [];
+var toDoId = 1;
+
+var markToDoAsCompleted = function (event) {
+    var inProgressElement = event.target;
+    var toDoToCompleteId = inProgressElement.getAttribute('data-todo-id');
+    var toDo = todos.find(function(todoElement) {
+        return todoElement.id == toDoToCompleteId;
+    });
+
+    toDo.isActive = false;
+
+    updateToDos();
+};
+
+var showCompletedToDos = function () {
+    var completedTodosList = document.querySelectorAll('#completed-todos ul')[0];
+    completedTodosList.innerHTML = '';
+
+    for (var i = 0; i < todos.length; i++) {
+        var currentToDo = todos[i];
+        if (!currentToDo.isActive) {
+            var listElement = document.createElement('li');
+            var toDoContent = document.createTextNode(currentToDo.description);
+            listElement.appendChild(toDoContent);
+            listElement.setAttribute('data-todo-id', currentToDo.id);
+            completedTodosList.appendChild(listElement);
+        }
+    }
+};
+
+var showInProgressToDos = function () {
+    var inProgressToDoList = document.querySelectorAll('#in-progress-todos ul')[0];
+    inProgressToDoList.innerHTML = '';
+
+    for (var i = 0; i < todos.length; i++) {
+        var currentToDo = todos[i];
+        if (currentToDo.isActive) {
+            var listElement = document.createElement('li');
+            var toDoContent = document.createTextNode(currentToDo.description);
+            listElement.appendChild(toDoContent);
+            listElement.setAttribute('data-todo-id', currentToDo.id);
+            inProgressToDoList.appendChild(listElement);
+            listElement.addEventListener('click', markToDoAsCompleted);
+        }
+    }
+};
+
+var updateToDos = function() {
+    showInProgressToDos();
+    showCompletedToDos();
+};
+
+var todoForm = document.getElementById('todo-form');
+todoForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    var todoTextBox = document.getElementById('new-todo');
+    var newToDo = todoTextBox.value;
+    
+    todos.push({
+        'id': toDoId,
+        'description': newToDo,
+        'isActive': true 
+    });
+    toDoId++;
+
+    todoTextBox.value = '';
+
+    updateToDos();
+});
+
+var resetButton = document.getElementById('reset-button');
+resetButton.addEventListener('click', function () {
+    todos = [];
+    updateToDos();
+});
